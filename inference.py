6eb5a465791fca9ac25f7538232e82dde8a7675a
srinandan0039-eng <srinandan0039@gmail.com>
2026-04-01 16:51:14 +0530

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,19 @@
+from waste_env import WasteEnv
+import random
+
+env = WasteEnv()
+
+state = env.reset()
+print("Initial State:", state)
+
+done = False
+
+while not done:
+    action = random.randint(0, 2)
+    state, reward, done, _ = env.step(action)
+
+    print("State:", state)
+    print("Reward:", reward)
+    print("-----")
+
+print("Finished")
