6c1bb5e298ba6b3fa7792e78cf877b2ec7771477
srinandan0039-eng <srinandan0039@gmail.com>
2026-04-01 16:54:09 +0530

    Create waste_env.py

---
+++ waste_env.py
@@ -0,0 +1,27 @@
+import random
+
+class WasteEnv:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.areas = [random.randint(20, 80) for _ in range(3)]
+        self.steps = 0
+        return self.areas
+
+    def step(self, action):
+        self.steps += 1
+
+        # clean selected area
+        self.areas[action] -= 10
+
+        # increase other areas
+        for i in range(3):
+            if i != action:
+                self.areas[i] += 5
+
+        # reward (less garbage is better)
+        reward = -sum(self.areas)
+
+        done = self.steps >= 10
+        return self.areas, reward, done, {}
